0a31fd634cca29c7804165027eb5114d91d41a5b
Bianca Oe <biancamsoe@gmail.com>
2011-11-13 17:28:02 -0200

    Todos os builtin

--- Versao2/Builtin.hpp
+++ Versao2/Builtin.hpp
@@ -18,6 +18,9 @@ void jobs (void) {
 	}
 }
 
+/*
+ *	Mostra comandos digitados ateh o momento
+ */
 void showHistory (void) {
 
 	std::list<std::string>::iterator curr, end;
@@ -25,6 +28,32 @@ void showHistory (void) {
 	for (curr = history.begin(), end = history.end(); curr != end; curr++) {
 		std::cout << *curr << std::endl;
 	}
+}
+
+/*
+ *	Envia um sinal SIGTERM para o job dado por id
+ *	
+ *	\Param
+ *		ID do job
+ *	\Exception
+ *		-id	Job inexistente
+ */
+void sendsigterm (int id) {
+	std::list<Job*>::iterator curr, end;
+	for (curr = jobList.begin(), end = jobList.end(); curr != end && (*curr)->getID() != id; curr++);
+	if (curr == end)	throw -id;
+	else
+		kill (-(*curr)->getPGID(), SIGTERM);
+}
+
+/*
+ *	Escreve os argumentos com 1 espaco entre eles
+ */
+void echo (int argc, char* args[]) {
+	int i;
+	for (i = 1; i < argc-1; i++)
+		printf ("%s ", args[i]);
+	printf ("%s\n", args[i]);
 }
 
 /*
@@ -139,7 +168,11 @@ int executeBuiltin (Process *p) {
 			std::cout << "fg: ID esperado" << std::endl;
 			return 1;
 		}
-		sscanf (cmd[1], "%d", &id);
+		if (cmd[1][0] != '%') {
+			std::cout << "fg: \% esperado" << std::endl;
+			return 1;
+		}
+		sscanf (cmd[1], "%*c%d", &id);
 		try {
 			foreground (id);
 		} catch (int err) {
@@ -151,7 +184,11 @@ int executeBuiltin (Process *p) {
 			std::cout << "bg: ID esperado" << std::endl;
 			return 1;
 		}
-		sscanf (cmd[1], "%d", &id);
+		if (cmd[1][0] != '%') {
+			std::cout << "bg: \% esperado" << std::endl;
+			return 1;
+		}
+		sscanf (cmd[1], "%*c%d", &id);
 		try {
 			background (id);
 		} catch (int err) {
@@ -170,8 +207,21 @@ int executeBuiltin (Process *p) {
 		changeDirectory (cmd[1]);
 	} else if (!strcmp (cmd[0], "history")) {
 		showHistory();
-	} else if (!strcmp (cmd[0], "fg")) {
-	} else if (!strcmp (cmd[0], "fg")) {
+	} else if (!strcmp (cmd[0], "echo")) {
+		echo (size, cmd);
+	} else if (!strcmp (cmd[0], "kill")) {
+		if (size < 2) {
+			std::cout << "kill: ID esperado" << std::endl;
+			return 1;
+		}
+		if (cmd[1][0] != '%')	return -1;
+		sscanf (cmd[1], "%*c%d", &id);
+		try {
+			sendsigterm (id);
+		} catch (int err) {
+			fprintf (stderr, "kill: %d inexistente\n", -err);
+			return 2;
+		}
 	} else if (!strcmp (cmd[0], "fg")) {
 	} else	return -1;
 	return 0;
--- Versao2/main.cpp
+++ Versao2/main.cpp
@@ -1,7 +1,10 @@
 #include "shooSH.hpp"
+#include <mcheck.h>
 
 int main (void) {
+	mtrace();
 	shooSH_init();
 	shooSH_run();
+	muntrace();
 	return 0;
 }
